9e45d1240287c97e549a61ea9964ed0aa867aae9
codesquezze <sahil1kumar2003@gmail.com>
2023-08-10 23:52:43 +0530

    final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
-
     return (
         <nav class={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
